92296745293ae9b339f096da67ee021119e68800
Kovalenko Konstantin <94693119+KovalenkoKonstantin@users.noreply.github.com>
2024-07-15 11:45:35 +0300

    employee automatic update

---
+++ Employee.m
@@ -0,0 +1,17 @@
+let
+    // Подключаемся к базе данных SQL Server
+    server = Sql.Database("msk-sql-02", "RKM"),
+
+    // Получаем значение параметра company_id из таблицы "Параметры" в текущей книге Excel
+    company_id = Number.ToText(Excel.CurrentWorkbook(){[Name="Параметры"]}[Content][company_id]{0}),
+
+    // Определяем имя хранимой процедуры, которую будем вызывать
+    query = "GetEmployeeRefresh",
+
+    // Выполняем хранимую процедуру с параметрами company_id и year_number
+    Источник = Value.NativeQuery(
+        server,
+        "exec " & query & " " & company_id & ""
+    )
+in
+    Источник
