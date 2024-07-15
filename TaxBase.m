1340d5714809ca581e39f5a40b7ceed5d00e0405
Kovalenko Konstantin <94693119+KovalenkoKonstantin@users.noreply.github.com>
2024-07-15 10:55:48 +0300

    tax base automatic update

---
+++ TaxBase.m
@@ -0,0 +1,28 @@
+// let
+//     Источник = Sql.Database("msk-sql-02", "RKM", [Query="execute GetTaxBaseRefresh 9, 2024;"])
+// in
+//     Источник
+
+let
+    // Подключаемся к базе данных SQL Server
+    server = Sql.Database("msk-sql-02", "RKM"),
+
+    // Получаем значение параметра start_year_number из таблицы "Параметры" в текущей книге Excel
+    start_year_number = Number.ToText(Excel.CurrentWorkbook(){[Name="Параметры"]}[Content][start_year_number]{0}),
+	
+	// Получаем значение параметра end_year_number из таблицы "Параметры" в текущей книге Excel
+    end_year_number = Number.ToText(Excel.CurrentWorkbook(){[Name="Параметры"]}[Content][end_year_number]{0}),
+
+    // Получаем значение параметра company_id из таблицы "Параметры" в текущей книге Excel
+    company_id = Number.ToText(Excel.CurrentWorkbook(){[Name="Параметры"]}[Content][company_id]{0}),
+
+    // Определяем имя хранимой процедуры, которую будем вызывать
+    query = "GetTaxBaseRefresh",
+
+    // Выполняем хранимую процедуру с параметрами company_id и year_number
+    Источник = Value.NativeQuery(
+        server,
+        "exec " & query & " " & company_id & ", " & start_year_number & "," & end_year_number & ""
+    )
+in
+    Источник
