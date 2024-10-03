ae987437d247973a67cfe228fee3e2e85d6c7ef6
Kovalenko Konstantin <94693119+KovalenkoKonstantin@users.noreply.github.com>
2024-10-03 16:02:59 +0300

    previous changes

--- TaxBase.m
+++ TaxBase.m
@@ -1,8 +1,3 @@
-// let
-//     Источник = Sql.Database("msk-sql-02", "RKM", [Query="execute GetTaxBaseRefresh 9, 2024;"])
-// in
-//     Источник
-
 let
     // Подключаемся к базе данных SQL Server
     server = Sql.Database("msk-sql-02", "RKM"),
@@ -16,13 +11,22 @@ let
     // Получаем значение параметра company_id из таблицы "Параметры" в текущей книге Excel
     company_id = Number.ToText(Excel.CurrentWorkbook(){[Name="Параметры"]}[Content][company_id]{0}),
 
+    // Получаем данные из умной таблицы "Сотрудники"
+    employeesTable = Excel.CurrentWorkbook(){[Name="Сотрудники"]}[Content],    
+    // Убираем строку с "Итого"
+    filteredEmployeesTable = Table.SelectRows(employeesTable, each [Сотрудник] <> "Итого"),
+    // Получаем только список сотрудников
+    employeeNamesList = filteredEmployeesTable[Сотрудник],    
+    // Конкатенируем имена сотрудников в одну строку, разделяя запятыми
+    employeeNames = Text.Combine(employeeNamesList, ","),
+
     // Определяем имя хранимой процедуры, которую будем вызывать
     query = "GetTaxBaseRefresh",
 
-    // Выполняем хранимую процедуру с параметрами company_id и year_number
+    // Формируем вызов хранимой процедуры с параметрами company_id, start_year_number, end_year_number, и employee_names
     Источник = Value.NativeQuery(
         server,
-        "exec " & query & " " & company_id & ", " & start_year_number & "," & end_year_number & ""
+        "exec " & query & " " & company_id & ", " & start_year_number & ", " & end_year_number & ", '" & employeeNames & "'"
     )
 in
     Источник
