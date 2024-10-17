6a02676fe277c6bdfed3535f579a9f0aee1c1f48
Kovalenko Konstantin <94693119+KovalenkoKonstantin@users.noreply.github.com>
2024-10-17 14:19:39 +0300

    VHI v.1.0

---
+++ VHI_v.1.0.m
@@ -0,0 +1,25 @@
+let
+    //v.1.0
+    // Подключение к SQL-серверу, указывая имя сервера и название базы данных
+    server = Sql.Database("msk-sql-02", "RKM"),
+    
+    // Извлекаем значение year_id из диапазона "Параметры" в текущем рабочем файле Excel
+    // Применяем функцию Number.ToText для преобразования в текстовую строку
+    year_id = Number.ToText(Excel.CurrentWorkbook(){[Name="Параметры"]}[Content][year_id]{0}),
+    
+    // Извлекаем значение company_id из диапазона "Параметры" в текущем рабочем файле Excel
+    // Применяем функцию Number.ToText для преобразования в текстовую строку
+    company_id = Number.ToText(Excel.CurrentWorkbook(){[Name="Параметры"]}[Content][id]{0}),
+    
+    // Форматируем дату в виде строки 'YYYY-MM-DD', представляющей 31 июля текущего года
+    // Получаем year_id и добавляем строку "-07-31" для обозначения конкретной даты
+    dat = "'" & Number.ToText(Excel.CurrentWorkbook(){[Name="Параметры"]}[Content][year_id]{0}) & "-07-31'",
+    
+    // Выполняем SQL-запрос к базе данных с помощью вызова функции Value.NativeQuery
+    Источник = Value.NativeQuery(
+        server,
+        "exec VHI_v_1_0 " & company_id & ", " & year_id & ", " & dat & ""
+    )
+in
+    // Возвращаем результат выполнения запроса (переменную "Источник")
+    Источник
