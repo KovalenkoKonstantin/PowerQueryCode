9da9a319c308a1fa6f9e47ec84ccbe49d10838ec
Kovalenko Konstantin <94693119+KovalenkoKonstantin@users.noreply.github.com>
2024-11-14 15:31:16 +0300

    ПМ

---
+++ ПМ_v.1.0.m
@@ -0,0 +1,23 @@
+let
+    // Источник данных
+    Источник = Excel.CurrentWorkbook(){[Name="Расходы_ПМ"]}[Content],
+    
+    // Фильтруем строки, чтобы удалить те, в которых в первом столбце содержится "Итого" и в столбце "Итого" значение равно 0
+    FilteredRows = Table.SelectRows(Источник, each [Итого] <> 0 and [Наименование отдела] <> null),
+    
+    // Удаляю всё лишнее
+    RemowedRows = Table.RemoveColumns(FilteredRows,{"№", "employee_accounting_type", "Август", "Сентябрь", "Октябрь", "Ноябрь", "Декабрь", "Январь", "Февраль", "Март", "Апрель", "Май", "Июнь", "Июль", "Итого"}),
+    
+    // Добавляем столбец "Месяц" и создаем список из 12 значений месяцев (1..12)
+    AddMonths = Table.AddColumn(RemowedRows, "Месяц", each List.Transform({1..12}, (month) => month)),
+
+    // Расширяем столбец "Месяц", чтобы дублировать строки для каждого месяца
+    ExpandedMonths = Table.ExpandListColumn(AddMonths, "Месяц"),
+
+    // Меняю название первого столбца на "ФИО"
+    Final = Table.RenameColumns(ExpandedMonths, {{"Наименование отдела", "Подразделение"}}),
+
+    //Преобразую месяц в число
+    MonthAsNumber = Table.TransformColumnTypes(Final,{{"Месяц", type number}})
+in
+    MonthAsNumber
